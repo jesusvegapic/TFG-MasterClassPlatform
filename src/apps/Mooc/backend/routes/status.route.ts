7132d2cc5f5a7c691c1792e81481d0277bb36574
Chus <jesusvegapic@gmail.com>
2022-02-19 20:45:46 +0100

    SyntaxError nameSpace StatusGetDController injector

--- src/apps/Mooc/backend/routes/status.route.ts
+++ src/apps/Mooc/backend/routes/status.route.ts
@@ -3,6 +3,6 @@ import container from '../dependency-injection';
 import StatusGetController from '../controllers/StatusGetController';
 
 export const register = (router: Router) => {
-  const controller: StatusGetController = container.get('Apps.mooc.controllers/StatusGetController')
+  const controller: StatusGetController = container.get('Apps.mooc.controllers.StatusGetController')
   router.get('/status', (req: Request, res: Response) => controller.run(req, res));
 };
