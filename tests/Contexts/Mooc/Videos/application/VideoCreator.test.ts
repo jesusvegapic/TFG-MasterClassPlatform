d67c0a3176ba68a92dccf5ed8f467c5ad4d05a2a
Jesús Vega <jesusvegapic@gmail.com>
2022-02-26 11:44:51 +0100

    Mooc Backend Unit Test application VideoCreator
    Cambios Realizados:
    Unit-Test 'should create a valid video'
    
    - Ahora la clase video debería tener el atributo url, donde se almacena.

--- tests/Contexts/Mooc/Videos/application/VideoCreator.test.ts
+++ tests/Contexts/Mooc/Videos/application/VideoCreator.test.ts
@@ -1,3 +1,6 @@
+import {VideoCreator} from "../../../../../src/Contexts/Mooc/Videos/application/VideoCreator"
+import {VideoRepository} from "../../../../../src/Contexts/Mooc/Videos/domain/VideoRepository"
+import {Video} from "../../../../../src/Contexts/Mooc/Videos/domain/Video"
 
 describe('VideoCreator', () => {
     it('should create a valid video', async () => {
@@ -8,9 +11,10 @@ describe('VideoCreator', () => {
         const id = 'id';
         const name = 'name';
         const duration = '5 hours';
-        const expectedVideo = new Video(id, name, duration);
+        const url = 'storageVideoUrl'
+        const expectedVideo = new Video(id, name, duration, url);
 
-        await creator.run(id, name , duration);
+        await creator.run(id, name , duration, url);
 
         expect(repository.save).toHaveBeenCalledWith(expectedVideo);
     });
