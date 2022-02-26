f17247192f049ff85dfcdde2bff5b43fabd35892
Jesús Vega <jesusvegapic@gmail.com>
2022-02-26 14:18:12 +0100

    Mooc Backend App Feature Create Video
    Cambios Realizados:
    
    - Inyectadas las dependencias de la aplicación a VideoPutController.
    
    - Implementación de VideoPutController terminada.
    
    Estado test de aceptación de la feature Subir Video VERDE.
    
    Ciclo ATDD verde de la feature Subir video completado.
    
    Refactor ATDD de la feature Subir video sin completar.
    
    Prototipo esqueleto de la feature Subir video completado.

--- src/apps/Mooc/backend/controllers/VideoPutController.ts
+++ src/apps/Mooc/backend/controllers/VideoPutController.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { VideoCreator } from "../../../../Contexts/Mooc/Videos/application/VideoCreator";
 import { Controller } from "./Controller";
 
 export class VideoPutController implements Controller {
+  constructor(private videoCreator: VideoCreator) {}
+
     async run(req: Request, res: Response): Promise<void> {
-      throw new Error('Method not implemented. ');
+      const {id, name, duration, url} = req.body;
+      
+      await this.videoCreator.run(id, name, duration, url);
+
+      res.status(httpStatus.CREATED).send();
     }
 }
